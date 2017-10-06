2996d62fb01d363bc6e196e034ffbcf85ab29978
Eric Siber <eric.siber@gmail.com>
2017-10-06 14:18:41 +0200

    Etapes 8 et 9

--- src/main/java/co/simplon/PoleEmploi/patrimoine/endpoint/VilleResource.java
+++ src/main/java/co/simplon/PoleEmploi/patrimoine/endpoint/VilleResource.java
@@ -5,6 +5,8 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import co.simplon.PoleEmploi.Application;
 import co.simplon.PoleEmploi.patrimoine.dao.VilleDao;
@@ -19,6 +21,7 @@ public class VilleResource {
 	}
 	
     @GET
+    @Produces(MediaType.APPLICATION_JSON)
     public List<Ville> getVilles() {
 		EntityManager em = createEntityManager();
 		VilleDao dao = new VilleJpaDao(em);
